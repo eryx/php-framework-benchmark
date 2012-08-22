bceefadd202b94178f98b2215d6b6a113a95e88e
eryx <evorui@gmail.com>
2012-08-22 18:14:01 +0800

    phalcon 0.4.4 added

---
+++ phalcon/app/controllers/IndexController.php
@@ -0,0 +1,8 @@
+<?php
+
+class IndexController extends Phalcon_Controller
+{
+    function indexAction()
+    {
+    }
+}
---
+++ phalcon/public/index.php
@@ -0,0 +1,51 @@
+<?php
+
+if (isset($_GET['debug'])) {
+    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+
+    define('START_TIME', microtime(true));
+    define('START_MEMORY_USAGE', memory_get_usage());
+}
+
+try {
+
+     $front = Phalcon_Controller_Front::getInstance();
+
+     // Setting directories
+     $front->setControllersDir("../app/controllers/");
+     $front->setModelsDir("../app/models/");
+     $front->setViewsDir("../app/views/");
+
+     // Printing view output
+     echo $front->dispatchLoop()->getContent();
+
+} catch(Phalcon_Exception $e) {
+     echo "PhalconException: ", $e->getMessage();
+}
+
+
+$xhprof_data = xhprof_disable();
+
+if (!isset($_GET['debug'])) {
+    die();
+}
+
+echo "Page rendered in <b>"
+    . round((microtime(true) - START_TIME), 5) * 1000 ." ms</b>, taking <b>"
+    . round((memory_get_usage() - START_MEMORY_USAGE) / 1024, 2) ." KB</b>";
+$f = get_included_files();
+echo ", include files: ".count($f);
+
+$XHPROF_ROOT = realpath(dirname(__FILE__) .'/../..');
+include_once $XHPROF_ROOT . "/xhprof/xhprof_lib/utils/xhprof_lib.php";
+include_once $XHPROF_ROOT . "/xhprof/xhprof_lib/utils/xhprof_runs.php";
+
+// save raw data for this profiler run using default
+// implementation of iXHProfRuns.
+$xhprof_runs = new XHProfRuns_Default();
+
+// save the run under a namespace "xhprof_foo"
+$run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_foo");
+
+echo ", xhprof <a href=\"http://xhprof.pfb.example.com/xhprof_html/index.php?run=$run_id&source=xhprof_foo\">url</a>";
+
