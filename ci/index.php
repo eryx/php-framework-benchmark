062d680a023fe607abbf378ca3c0b919b0c85954
eryx <evorui@gmail.com>
2012-12-18 16:30:36 +0800

    Upgrade CI to 2.1.3

--- ci/index.php
+++ ci/index.php
@@ -24,7 +24,7 @@ if (isset($_GET['debug'])) {
  * NOTE: If you change these, also change the error_reporting() code below
  *
  */
-	define('ENVIRONMENT', 'development');
+	define('ENVIRONMENT', 'production');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
